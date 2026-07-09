1a5dd688570735bfee8979a883244ddd9eb6b34b
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-09 12:38:23 +0100

    Add files via upload

---
+++ pack.py
@@ -0,0 +1,24 @@
+import os
+import copy
+print("\033c\033[47;30m\ngive me the .txt pack file ? \n")
+a=input().strip()
+f1=open(a,"r")
+f=f1.read()
+f1.close()
+ff=f.split("\n")
+names=ff[0]
+files=ff[1]
+counter=0
+f1=open(names+".pack1","wb")
+f1.close()
+
+for d in ff:
+    if counter!=0 and d.strip()!="":
+        f1=open(d,"rb")
+        fff=f1.read()
+        f1.close()
+        f1=open(names+".pack1","ba")
+        f1.write(fff)
+        f1.write(b"\x01\x00\x05\x04\x03\x02")
+        f1.close()
+    counter=counter+1
