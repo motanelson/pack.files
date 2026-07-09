f4705998f28449f3e9b7cdc1468880c1fa321f8b
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-09 12:37:42 +0100

    Delete c.sh

--- c.sh
+++
@@ -1,5 +0,0 @@
-printf "\ec\e[47;31m\ngive me c file?\n"
-read a
-gcc -fPIC -fPIE -pie -fpic -m64 -fno-stack-protector -fomit-frame-pointer -finline-functions -fno-zero-initialized-in-bss -o $a.elf $a
-
-
