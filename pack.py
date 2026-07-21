693315ef36a85989a4fc7a149267cd4d961d420f
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-21 16:27:58 +0100

    Add files via upload

--- pack.py
+++ pack.py
@@ -1,7 +1,11 @@
 import os
 import copy
+import zlib
+from tkinter import filedialog
+
 print("\033c\033[47;30m\ngive me the .txt pack file ? \n")
-a=input().strip()
+a=filedialog.askopenfile(title="give me the .txt pack file  ? ",defaultextension="*.txt")
+a=a.name
 f1=open(a,"r")
 f=f1.read()
 f1.close()
@@ -18,7 +22,7 @@ for d in ff:
         fff=f1.read()
         f1.close()
         f1=open(names+".pack1","ba")
-        f1.write(fff)
+        f1.write(zlib.compress(fff,level=9))
         f1.write(b"\x01\x00\x05\x04\x03\x02")
         f1.close()
     counter=counter+1
